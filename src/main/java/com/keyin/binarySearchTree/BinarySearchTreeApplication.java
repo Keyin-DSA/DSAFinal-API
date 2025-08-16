bfbb204837efac53ad40c81d8d1a91442080e8cb
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-08-15 21:36:29 -0230

    Add initial implementation of BinarySearchTree and Node classes
    - Create `Node` class to represent tree nodes.
    - Introduce `BinarySearchTree` class with stubbed methods for insertion, search, deletion, and traversal.
    - Update `BinarySearchTreeApplication` to include an instance of `BinarySearchTree`.

--- src/main/java/com/keyin/binarySearchTree/BinarySearchTreeApplication.java
+++ src/main/java/com/keyin/binarySearchTree/BinarySearchTreeApplication.java
@@ -1,5 +1,6 @@
 package com.keyin.binarySearchTree;
 
+import com.keyin.binarySearchTree.brocode.BinarySearchTree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BinarySearchTreeApplication {
 
 	public static void main(String[] args) {
+
+		BinarySearchTree tree = new BinarySearchTree();
+
 		SpringApplication.run(BinarySearchTreeApplication.class, args);
 	}
 
