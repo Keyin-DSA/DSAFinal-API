0a90eaa821c85713d27680b3da351b29ecef6904
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-08-15 21:49:15 -0230

    Implement `insert` and `display` methods in BinarySearchTree
    - Add recursive helper method for `insert` to enable node addition to the tree.
    - Implement in-order traversal in `display` to print tree data.

--- src/main/java/com/keyin/binarySearchTree/brocode/BinarySearchTree.java
+++ src/main/java/com/keyin/binarySearchTree/brocode/BinarySearchTree.java
@@ -5,13 +5,34 @@ public class BinarySearchTree {
     Node root;
 
     public void insert(Node node) {
+        root = insertHelper(root, node);
 
     }
     private Node insertHelper(Node root, Node node){
-        return null;
+        int data = node.data;
+        if(root == null){
+            root = node;
+            return root;
+        }
+        else if(data < root.data){
+            root.left = insertHelper(root.left, node);
+        }
+        else {
+            root.right = insertHelper(root.right, node);
+        }
+        return root;
     }
-    private void display() {}
-    private void displayHelper(Node root){}
+    public void display() {
+        displayHelper(root);
+    }
+    private void displayHelper(Node root){
+        if(root == null){
+            return;
+        }
+        displayHelper(root.left);
+        System.out.println(root.data);
+        displayHelper(root.right);
+    }
 
     public boolean search(int data){
         return false;
